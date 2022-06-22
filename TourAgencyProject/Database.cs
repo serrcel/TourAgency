d97e2ecaa649cd6ccc703ad9db07735225fb4ed5
Соловьев Роман <serrcel@gmail.com>
2022-06-22 23:23:46 +0400

    Database add
    Добавил базу данных

---
+++ TourAgencyProject/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace TourAgencyProject
+{
+    public class Database
+    {
+        private SqlConnection sqlConnection;
+        string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+        public void Open()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+        }
+
+        public void Close()
+        {
+            if (sqlConnection != null)
+                sqlConnection.Close();
+            else
+                return;
+        }
+    }
+}
