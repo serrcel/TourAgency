d7893734c4367debb46b1c2ff4b20853299e067d
Соловьев Роман <serrcel@gmail.com>
2022-06-22 12:17:27 +0400

    Added database
    Добавил базу данных и настроил строку подключения

--- TourAgencyProject/MainWindow.xaml.cs
+++ TourAgencyProject/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.IO;
+
 namespace TourAgencyProject
 {
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            // Получает относительный путь до БД
+            string path = Directory.GetCurrentDirectory();
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+
             InitializeComponent();
         }
     }
